76f241758c8bbe4c7eb1b6d6f6b38eea4b907ebf
shubhang-dvivedi <shubhang20474@iiitd.ac.in>
2023-06-28 16:16:56 +0530

    add pieceHeatMap function in evaluateBoard

--- src/app/computeNextMove.js
+++ src/app/computeNextMove.js
@@ -1,40 +1,20 @@
+import { evaluateBoard } from './evaluateBoard.js';
+
 // functions to be implemented
-function rules(position, piece_coordinate, target_coordinate){}
+function rules(board, piece_position, target_position){}
 
-// INPUT board position and RETURNS the evaluation of the board
-// evaluation parameters: piece points. (add more parameters)
-function evaluatePosition(position){
-    const piece_points = {'p':100,'P':100,'b':300,'B':300,'n':300,'N':300,'r':500,'R':500,'q':900,'Q':900,'k':1000,'K':1000};
-    let white_points = 0;
-    let black_points = 0;
-    for (let i = 0; i < 8; i++){
-        for (let j = 0; j < 8; j++){
-            if (position[i][j] != ''){
-                let piece = position[i][j];
-                if (piece === toUpperCase(piece)){
-                    white_points += piece_points[piece];
-                } else {
-                    black_points += piece_points[piece];
-                }
-            }
-        }
-    }
-    let eval = white_points - black_points;
-    return eval;
-}
-
-// INPUTS board position and player to move
+// INPUTS board and player to move
 // RETURNS true if game is over
-function isGameOver(position, player){
+function isGameOver(board, player){
     const white_player = (player == 'w') ? true : false;
     for (let i = 0; i < 8; i++){
         for (let j = 0; j < 8; j++){
-            let piece = position[i][j];
+            let piece = board[i][j];
             if ((white_player && piece === toUpperCase(piece)) || (!white_player && piece === toLowerCase(piece))){
                 for (let m = 0; m < 8; m++){
                     for (let n = 0; n < 8; n++){
-                        if (rules(position, [i,j], [m,n])) {
-                            return false; // CORNER CASE: check for if the game is draw
+                        if (rules(board, [i,j], [m,n])) {
+                            return false; // CORNER CASE: check if the game is draw
                         }
                     }
                 }
@@ -44,19 +24,19 @@ function isGameOver(position, player){
     return true;
 }
 
-// findAllMoves find all possible moves of a certain player given a board position
-// INPUT board position and player to move and RETURNS an array of board positions that can be achieved after player has played all possible moves
-function findAllMoves(position, player){
+// findAllMoves find all possible moves of a certain player given a board board
+// INPUT board and player to move and RETURNS an array of boards that can be achieved after player has played all possible moves
+function findAllMoves(board, player){
     const white_player = (player == 'w') ? true : false;
     const all_moves = [];
     for (let i = 0; i < 8; i++){
         for (let j = 0; j < 8; j++){
-            let piece = position[i][j];
+            let piece = board[i][j];
             if ((white_player && piece === toUpperCase(piece)) || (!white_player && piece === toLowerCase(piece))){
                 for (let m = 0; m < 8; m++){
                     for (let n = 0; n < 8; n++){
-                        if (rules(position, [i,j], [m,n])) {
-                            let temp = position;
+                        if (rules(board, [i,j], [m,n])) {
+                            let temp = board; // handle edge cases: castling, en passant, pawn promotion
                             if ((piece === 'K' || piece === 'k') && Math.abs(j-n) == 2){
                                 temp[m][n] = temp[i][j];
                                 temp[i][j] = '';
@@ -82,28 +62,28 @@ function findAllMoves(position, player){
 }
 
 // nextBestMove uses minimax algo and alpha beta pruning to compute best possible move.
-// INPUT search depth, board position, player to move, alpha and beta (parameters for alpha beta pruning)
-// RETURNS an array which consists of [position evaluation after search depth if next best move played, board position after next best move]
-function nextBestMove(depth, position, player, alpha, beta){
+// INPUT search depth, board board, player to move, alpha and beta (parameters for alpha beta pruning)
+// RETURNS an array which consists of [board evaluation after search depth if next best move played, board board after next best move]
+function nextBestMove(depth, board, player, alpha, beta){
     const white_player = (player == 'w') ? true : false;
-    if (isGameOver(position, (white_player)?'w':'b' )){
+    if (isGameOver(board, (white_player)?'w':'b' )){
         if (white_player){
-            return [-1e9, position];
+            return [-1e9, board];
         } else {
-            return [1e9, position];
+            return [1e9, board];
         }
     }
     if (depth == 0){
-        return [evaluatePosition(position), position];
+        return [evaluateBoard(board), board];
     }
 
     if (white_player){
         max_eval = -1e9;
-        let new_positions = findAllMoves(position, white_player);
-        for (let new_pos of new_positions){
-            let eval = nextBestMove(depth-1, new_pos, false, alpha, beta);
-            if (eval[0] > max_eval[0]) max_eval = [eval[0], new_pos];
-            alpha = max(alpha, eval[0]);
+        let new_boards = findAllMoves(board, white_player);
+        for (let new_pos of new_boards){
+            let evaluation = nextBestMove(depth-1, new_pos, false, alpha, beta);
+            if (evaluation[0] > max_eval[0]) max_eval = [evaluation[0], new_pos];
+            alpha = max(alpha, evaluation[0]);
             if (beta <= alpha){
                 break;
             }
@@ -111,11 +91,11 @@ function nextBestMove(depth, position, player, alpha, beta){
         return max_eval;
     } else{
         min_eval = 1e9;
-        let new_positions = findAllMoves(position, white_player);
-        for (let new_pos of new_positions){
-            let eval = nextBestMove(depth-1, new_pos, true, alpha, beta);
-            if (eval[0] < min_eval[0]) min_eval = [eval[0], new_pos];
-            beta = min(beta, eval[0]);
+        let new_boards = findAllMoves(board, white_player);
+        for (let new_pos of new_boards){
+            let evaluation = nextBestMove(depth-1, new_pos, true, alpha, beta);
+            if (evaluation[0] < min_eval[0]) min_eval = [evaluation[0], new_pos];
+            beta = min(beta, evaluation[0]);
             if (beta <= alpha){
                 break;
             }
@@ -125,11 +105,11 @@ function nextBestMove(depth, position, player, alpha, beta){
 }
 
 // computes the next best move
-// takes INPUT positions, depth of search, player to move and RETURNS board position after next move
+// takes INPUT boards, depth of search, player to move and RETURNS board board after next move
 // the next move generated is currently finding the next best possible move
-function computeNextMove(position, depth = 2, player){
+export function computeNextMove(board, depth = 2, player){
     let alpha = -1e9;
     let beta = 1e9;
-    const next_move = nextBestMove(depth, position, player, alpha, beta)[1];
+    const next_move = nextBestMove(depth, board, player, alpha, beta)[1];
     return next_move;
 }
---
+++ src/app/evaluateBoard.js
@@ -0,0 +1,112 @@
+function pieceHeatMap(board, piece, position){
+    const pawn_map =
+    [[0,0,0,0,0,0,0,0],
+    [50,50,50,50,50,50,50,50],
+    [10,10,20,30,30,20,10,10],
+    [5,5,10,25,25,10,5,5],
+    [0,0,0,20,20,0,0,0],
+    [5,-5,-10,0,0,-10,-5,5],
+    [5,10,10,-20,-20,10,10,5],
+    [0,0,0,0,0,0,0,0]];
+
+    const knight_map =
+    [[-50,-40,-30,-30,-30,-30,-40,-50],
+    [-40,-20,0,0,0,0,-20,-40],
+    [-30,0,10,15,15,10,0,-30],
+    [-30,5,15,20,20,15,5,-30],
+    [-30,0,15,20,20,15,0,-30],
+    [-30,5,10,15,15,10,5,-30],
+    [-40,-20,0,5,5,0,-20,-40],
+    [-50,-40,-30,-30,-30,-30,-40,-50],];
+
+    const bishop_map =
+    [[-20,-10,-10,-10,-10,-10,-10,-20],
+    [-10,0,0,0,0,0,0,-10],
+    [-10,0,5,10,10,5,0,-10],
+    [-10,5,5,10,10,5,5,-10],
+    [-10,0,10,10,10,10,0,-10],
+    [-10,10,10,10,10,10,10,-10],
+    [-10,5,0,0,0,0,5,-10],
+    [-20,-10,-10,-10,-10,-10,-10,-20]];
+
+    const rook_map =
+    [[0,0,0,0,0,0,0,0],
+    [5,10,10,10,10,10,10,5],
+    [-5,0,0,0,0,0,0,-5],
+    [-5,0,0,0,0,0,0,-5],
+    [-5,0,0,0,0,0,0,-5],
+    [-5,0,0,0,0,0,0,-5],
+    [-5,0,0,0,0,0,0,-5],
+    [0,0,0,5,5,0,0,0]];
+
+    const queen_map =
+    [[-20,-10,-10,-5,-5,-10,-10,-20],
+    [-10,0,0,0,0,0,0,-10],
+    [-10,0,5,5,5,5,0,-10],
+    [-5,0,5,5,5,5,0,-5],
+    [0,0,5,5,5,5,0,-5],
+    [-10,5,5,5,5,5,0,-10],
+    [-10,0,5,0,0,0,0,-10],
+    [-20,-10,-10,-5,-5,-10,-10,-20]];
+
+    const king_map =
+    [[-30,-40,-40,-50,-50,-40,-40,-30],
+    [-30,-40,-40,-50,-50,-40,-40,-30],
+    [-30,-40,-40,-50,-50,-40,-40,-30],
+    [-30,-40,-40,-50,-50,-40,-40,-30],
+    [-20,-30,-30,-40,-40,-30,-30,-20],
+    [-10,-20,-20,-20,-20,-20,-20,-10],
+    [20,20,0,0,0,0,20,20],
+    [20,30,10,0,0,10,30,20]];
+
+    let x = position[0];
+    let y = position[1];
+    if (piece === piece.toLowerCase){
+        x = 7-x;
+    }
+    switch(toUpperCase(piece)){
+        case 'P':
+            return pawn_map[x][y];
+        case 'N':
+            return knight_map[x][y];
+        case 'B':
+            return bishop_map[x][y];
+        case 'R':
+            return rook_map[x][y];
+        case 'Q':
+            return queen_map[x][y];
+        case 'K':
+            return king_map[x][y];
+        default:
+            console.log("Incorrect piece name");
+            break;
+    }
+}
+
+function totalPiecePoints(board){
+    const piece_points = {'p':100,'P':100,'b':300,'B':300,'n':300,'N':300,'r':500,'R':500,'q':900,'Q':900,'k':1e9,'K':1e9};
+    let white_points = 0;
+    let black_points = 0;
+    for (let i = 0; i < 8; i++){
+        for (let j = 0; j < 8; j++){
+            if (board[i][j] != ''){
+                let piece = board[i][j];
+                if (piece === toUpperCase(piece)){
+                    white_points += piece_points[piece];
+                    white_points += pieceHeatMap(board, piece, [i,j]);
+                } else {
+                    black_points += piece_points[piece];
+                    black_points += pieceHeatMap(board, piece, [i,j]);
+                }
+            }
+        }
+    }
+    return white_points - black_points;
+}
+
+// INPUT board board and RETURNS the evaluation of the board
+// evaluation parameters: piece points, board of piece on board (add more parameters)
+export function evaluateBoard(board){
+    let final_eval = 0;
+    final_eval += totalPiecePoints(board);
+}
